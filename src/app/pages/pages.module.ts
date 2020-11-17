0c59068b3c572f91d73007dda9176900f24a021c
Luisen Poy <kttweb@gmail.com>
2020-11-17 19:34:05 +0100

    creados modulo y components shared y auth

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,18 +1,28 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { SharedpagesModule } from './sharedpages/sharedpages.module';
+
 import { AboutComponent } from './about/about.component';
 import { WorksComponent } from './works/works.component';
 import { ContactComponent } from './contact/contact.component';
 import { WorkDetailComponent } from './work-detail/work-detail.component';
-import { HeaderComponent } from './shared/header/header.component';
-import { FooterComponent } from './shared/footer/footer.component';
-
-
+import { PagesComponent } from './pages.component';
 
 @NgModule({
-  declarations: [AboutComponent, WorksComponent, ContactComponent, WorkDetailComponent, HeaderComponent, FooterComponent],
-  imports: [
-    CommonModule
-  ]
+  declarations: [
+    AboutComponent,
+    WorksComponent,
+    ContactComponent,
+    WorkDetailComponent,
+    PagesComponent,
+  ],
+  imports: [CommonModule, SharedpagesModule],
+  exports: [
+    AboutComponent,
+    WorksComponent,
+    ContactComponent,
+    WorkDetailComponent,
+    PagesComponent,
+  ],
 })
-export class PagesModule { }
+export class PagesModule {}
